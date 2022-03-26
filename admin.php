251a53f4254260939ab63009e38287a755bda984
Ramiro Molina Zeballos <ramiromolinazeballos@gmail.com>
2022-03-25 21:55:38 -0400

    Diseño Página Usuario Administrador de Sistema

--- admin.php
+++ admin.php
@@ -52,92 +52,115 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="css/admin.css">
   </head>
   <body>
-      <div class="wrapper">
-        <header>
-          <nav>
-            <div class="logo" style="width: 250px;">
-              <div class="col-div-6">
-              <span style="font-size:30px;cursor:pointer; color: white;" class="nav"  ><p class="logo"><span style="color: #f7403b;">R</span>-Rudy</p></span>
-              </div>
-            </div>
+    <div class="side-menu">
 
-            <div class="menu">
-              <ul>
-                <li><a href="admin.php">Inicio</a></li>
-                <li><a href="login.php?close" class="black">Cerrar sesión</a></li>
-              </ul>
-            </div>
-          </nav>
-        </header>
+      <div class="brand-img">
+        <img src="css/img/user.png" alt="" class="brand-logo">
       </div>
 
-    <div class="container" style="width:1000px;">
+      <div class="brand-name">
+        <h2>Ramiro</h2> 
+      </div>
 
-      <div class="col-md-3">
-           <input type="text" name="userlook" id="cilook" class="form-control" placeholder="CI Usuario" />
+      <div class="brand-surname">
+        <h2>Molina Zeballos</h2> 
       </div>
-      <div class="col-md-5">
-           <input type="button" name="filte" id="filte" value="Buscar" class="btn btn-info" />
-           <input type="button" name="habilitados"  id="habilitados" value="Habilitados" class="btn btn-info" />
-           <input type="button" name="deshabilitados"  id="deshabilitados" value="Deshabilitados" class="btn btn-info" />
-           <a href="adminAdd.php">
-           <input type="button" name="addUser"  value="Agregar" class="btn btn-info">
-           </a>
-      </div>
+
+      <div class="brand-bar"></div>
+
+      <ul>
+        <a href="admin.php"><li><img src="css/img/people.png" alt="" class="list-nav">&nbsp; <span>Usuarios</span></li></a>
+        <a href="#"><li><img src="css/img/provider.png" alt="" class="list-nav">&nbsp;<span>Proveedores</span></li></a>
+      </ul>
     </div>
 
-    <div class="seccion">
-      <form>
-        <h2></h2>
-        <div id="order_table">
-          <table class="content-table">
-            <thead>
-              <tr class="active-row">
-                <th class="bg-primary" scope="col">ID</th>
-                <th class="bg-primary" scope="col">USUARIO</th>
-                <th class="bg-primary" scope="col">PASSWORD</th>
-                <th class="bg-primary" scope="col">NOMBRE</th>
-                <th class="bg-primary" scope="col">CI</th>
-                <th class="bg-primary" scope="col">ACCESO</th>
-                <th class="bg-primary" scope="col">ESTADO</th>
-                <th class="bg-primary" scope="col"></th>
-              </tr>
-            </thead>
+    <div class="container">
+      <div class="header">
+        <div class="nav">
+          <div class="nav-logo">
+            <p>RU <span class="namelog">DY</span></p>
+          </div>
 
-            <?php
-            foreach($result as $res):?>
-            <tbody>
-              <tr>
-                <td><?php echo $res["id_user"]; ?></td>
-                <td><?php echo $res["user_user"]; ?></td>
-                <td><?php echo $res["password_user"]; ?></td>
-                <td><?php echo $res["name_user"]; ?></td>
-                <td><?php echo $res["ci_user"]; ?></td>
-                <td><?php echo $res["access_user"]; ?></td>
-                <td><?php echo $res["state_user"]; ?></td>
-                <td>
-                  <a href="admin.php?userState=<?php echo $res['id_user']; ?>" class="btnAction btn btn-info">
-                    <?php
-                      if($res["state_user"] == 'habilitado'){
-                        echo "Deshabilitar";
-                      }
-                      else{
-                        echo "Habilitar";
-                      }
-                    ?>
-                  </a>
-                  <a href="adminUpdate.php?userList=<?php echo $res['id_user']; ?>" class="btnAction btn btn-info">Editar</a>
-                  <a href="function.php?userDelete=<?php echo $res['id_user']; ?>" class="btnAction btn btn-info">Eliminar</a>
-                </td>
-              </tr>
-            </tbody>
-            <?php endforeach ?>
-          </table>
+          <div class="nav-logout">
+            <a href="login.php?close" class="btn-logout"><li>LOGOUT &nbsp; <img src="css/img/logout.png" alt="" class="list-nav"></li></a>
+          </div>
         </div>
-      </form>
+      </div>
+
+      <div class="content">
+        <div class="menu-nav">
+          <div class="col-md-3">
+            <input type="text" name="userlook" id="cilook" class="form-control" placeholder="CI Usuario" />
+          </div>
+          
+          <div>
+          <input type="image" name="filte" id="filte" value="Buscar" class="btn menu-search " src="css/img/search.png" />
+          </div>
+
+          <div class="col-md-5">
+            <input type="button" name="habilitados"  id="habilitados" value="Habilitados" class="btn btn-info" />
+            <input type="button" name="deshabilitados"  id="deshabilitados" value="Deshabilitados" class="btn btn-info" />
+            <a href="adminAdd.php">
+            <input type="button" name="addUser"  value="Agregar" class="btn btn-info">
+            </a>
+          </div>
+        </div>
+
+        <div class="seccion">
+          <form>
+            <h2></h2>
+            <div id="order_table">
+              <table class="content-table">
+                <thead>
+                  <tr class="active-row">
+                    <th class="bg-primary" scope="col">USUARIO</th>
+                    <th class="bg-primary" scope="col">PASSWORD</th>
+                    <th class="bg-primary" scope="col">NOMBRE</th>
+                    <th class="bg-primary" scope="col">CI</th>
+                    <th class="bg-primary" scope="col">ACCESO</th>
+                    <th class="bg-primary" scope="col">ESTADO</th>
+                    <th class="bg-primary" scope="col">OPCIONES</th>
+                  </tr>
+                </thead>
+
+                <?php
+                foreach($result as $res):?>
+                <tbody>
+                  <tr>
+                    <td><?php echo $res["user_user"]; ?></td>
+                    <td><?php echo $res["password_user"]; ?></td>
+                    <td><?php echo $res["name_user"], " ", $res["surname_user"];?></td>
+                    <td><?php echo $res["ci_user"]; ?></td>
+                    <td><?php echo $res["access_user"]; ?></td>
+                    <td><?php echo $res["state_user"]; ?></td>
+                    <td>
+                      <a href="admin.php?userState=<?php echo $res['id_user']; ?>" class="btnAction btn btn-info">
+                        <?php
+                          if($res["state_user"] == 'habilitado'){
+                            echo "Deshabilitar";
+                          }
+                          else{
+                            echo "Habilitar";
+                          }
+                        ?>
+                      </a>
+                      <a href="adminUpdate.php?userList=<?php echo $res['id_user']; ?>" class="btnAction btn btn-info">Editar</a>
+                      <a href="function.php?userDelete=<?php echo $res['id_user']; ?>" class="btnAction btn btn-info">Eliminar</a>
+                    </td>
+                  </tr>
+                </tbody>
+                <?php endforeach ?>
+              </table>
+            </div>
+          </form>
+        </div>  
+      </div>
     </div>
+
+
   </body>
 </html>
 
